a68d50a5dde061c0ef9033b4006f767397b1bc13
Christian Schlinkmann <cschlinkmann@gmail.com>
2016-02-06 23:12:51 +0100

    Add a new Color By dropdown with working colors for Economy, Government and Allegiance

--- app/scripts/directives/edSystemMap/edSystemMap.js
+++ app/scripts/directives/edSystemMap/edSystemMap.js
@@ -3,6 +3,12 @@ angular.module('edGalaxyMap')
 			return {
 				restrict: 'E',
 				link: function (scope, elem, attr) {
+					var map_colorTypes = ["economy", "allegiance", "government"];
+					// The positions of these things need to match their color according to the palette in models/system_color_palette.png
+					var map_economy = ["None", "Extraction", "Refinery", "Industrial", "UNUSED", "Agriculture", "UNUSED", "Terraforming", "UNUSED", "High Tech", "Colony", "Service", "Tourism", "Military"];
+					var map_government = ["None", "Confederacy", "Prison Colony", "Anarchy", "Colony", "Democracy", "Imperial", "Corporate", "Communism", "Feudal", "Dictatorship", "Theocracy", "Cooperative", "Patronage"];
+					var map_allegiance = ["None", "Federation", "UNUSED", "Independent", "UNUSED", "UNUSED", "Alliance", "UNUSED", "UNUSED", "Empire", "UNUSED", "UNUSED", "UNUSED", "UNUSED"];
+
 					window.scene;
 					var camera;
 					var controls;
@@ -28,9 +34,7 @@ angular.module('edGalaxyMap')
 					var POP_SIZE_THRESHOLD = 1000000000 //population > than this will start scaling point size up
 					var c_pickingEnabled = true;
 					var c_mouseDownPos = new THREE.Vector2();
-					var targetLineMaterial = new THREE.LineBasicMaterial({
-              color: '0xffffff'
-          });
+					var targetLineMaterial = new THREE.LineBasicMaterial({color: '0xffffff'});
 					var label = $("#pointer");
 					var INTERSECTED;
 					//load galaxy data
@@ -39,11 +43,20 @@ angular.module('edGalaxyMap')
 					init();
 					animate();
 
-          scope.$on('selectedSystem:update', function(event,data) {
-            scope.selectedSystem = data;
-						selectedSystemIcon.visible = false;
-						flyToSystem(scope.selectedSystem);
-         });
+				scope.$on('selectedSystem:update', function(event,data) {
+					scope.selectedSystem = data;
+					selectedSystemIcon.visible = false;
+					flyToSystem(scope.selectedSystem);
+				});
+
+				scope.$on('systemColoring:update', function(event, newColorKey) {
+					var colorIndex = map_colorTypes.indexOf(newColorKey);
+					if (colorIndex < 0) {
+						console.error("Unknown system coloring type: "+newColorKey);
+						colorIndex = 0;
+					}
+					uniforms.activeColoring.value = colorIndex;
+				});
 
 					//wait for systems data to load, then draw systems and animate
 					scope.$watch(function() {
@@ -69,14 +82,16 @@ angular.module('edGalaxyMap')
 
 					function loadSystems() {
 						toggleSceneLoading(false);
+						var paletteTexture = THREE.ImageUtils.loadTexture('models/system_color_palette.png');
+						paletteTexture.minFilter = THREE.NearestFilter;
 						var texture = THREE.ImageUtils.loadTexture('models/circle.png');
 						texture.minFilter = THREE.LinearFilter;
 						uniforms = {
 
-							color:     { type: "c", value: new THREE.Color( 0xffffff ) },
-							texture:   { type: "t", value: texture  },
-							scale: {type: "f", value: 1.0},
-							size: { type: "f", value: 100}
+							activeColoring: { type: "i", value: 2 },
+							colorPalette: { type: "t", value: paletteTexture },
+							texture: { type: "t", value: texture },
+							scale: {type: "f", value: 1.0}
 
 						};
 
@@ -87,76 +102,35 @@ angular.module('edGalaxyMap')
 							fragmentShader: document.getElementById( 'fragmentshader' ).textContent,
 							depthTest:      true,
 							depthWrite:     true,
-							transparent:    false
+							transparent:    false,
+							shading:        THREE.FlatShading
 						});
-
-
 
 						geometry = new THREE.BufferGeometry();
 
+						/*
+						* This maps the property to a color index that will be used in the shader to read the right color from a texture palette
+						* colorIndex (vec4)
+						*   [0] = map_economy , range [0,10]
+						*   [1] = map_allegiance
+						*   [2] = map_government
+						*   [3] = unused (star type?)
+						*/
+						var colorIndex = new Float32Array( systemsService.systems.length * 4 );
 						var positions = new Float32Array( systemsService.systems.length * 3 );
-						var colors = new Float32Array( systemsService.systems.length * 3 );
 						var sizes = new Float32Array( systemsService.systems.length );
-
-						var color = new THREE.Color();
-
-						var extractionColor = 0xfe0000;
-						var refineryColor = 0xff7f00;
-						var industrialColor = 0xffff00;
-						var agricultureColor = 0x7fff00;
-						var terraFormingColor = 0x009901;
-						var highTechColor = 0x01ffff;
-						var colonyColor = 0x337eff;
-						var serviceColor = 0x0145ff;
-						var tourismColor = 0x6601e5;
-						var militaryColor = 0xe600e6;
-						var noEconomyColor = 0x666666;
 
 						for ( var i = 0, i3 = 0; i < systemsService.systems.length; i ++, i3 += 3 ) {
 
-							var currentEconomy = systemsService.systems[i].primary_economy;
 							var system = systemsService.systems[i];
 							positions[ i3 + 0 ] = system.x;
 							positions[ i3 + 1 ] = system.y;
 							positions[ i3 + 2 ] = system.z;
 
-							if(currentEconomy == 'Extraction'){
-								color.setHex( extractionColor );
-							}
-							else if(currentEconomy == 'Refinery'){
-									color.setHex( refineryColor );
-							}
-							else if(currentEconomy == 'Industrial'){
-										color.setHex( industrialColor );
-							}
-							else if(currentEconomy == 'Agriculture'){
-										color.setHex( agricultureColor );
-							}
-							else if(currentEconomy == 'Terraforming'){
-										color.setHex( terraFormingColor );
-							}
-							else if(currentEconomy == 'High Tech'){
-										color.setHex( highTechColor );
-							}
-							else if(currentEconomy == 'Colony'){
-										color.setHex( colonyColor );
-							}
-							else if(currentEconomy == 'Service'){
-										color.setHex( serviceColor );
-							}
-							else if(currentEconomy == 'Tourism'){
-										color.setHex( tourismColor );
-							}
-							else if(currentEconomy == 'Military'){
-										color.setHex( militaryColor );
-							}
-							else{
-									color.setHex( noEconomyColor );
-							}
-
-							colors[ i3 + 0 ] = color.r;
-							colors[ i3 + 1 ] = color.g;
-							colors[ i3 + 2 ] = color.b;
+							colorIndex[ i*4 + 0 ] = map_economy.indexOf(systemsService.systems[i].primary_economy);
+							colorIndex[ i*4 + 1 ] = map_allegiance.indexOf(systemsService.systems[i].allegiance);
+							colorIndex[ i*4 + 2 ] = map_government.indexOf(systemsService.systems[i].government);
+							colorIndex[ i*4 + 3 ] = 0;
 
 							sizes[ i ] = getPopulationScaleForSystem(system);
 
@@ -164,7 +138,7 @@ angular.module('edGalaxyMap')
 
 
 						geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
-						geometry.addAttribute( 'customColor', new THREE.BufferAttribute( colors, 3 ) );
+						geometry.addAttribute( 'aColorIndex', new THREE.BufferAttribute( colorIndex, 4 ) );
 						geometry.addAttribute( 'aSize', new THREE.BufferAttribute( sizes, 1 ) );
 						geometry.attributes.aSize.needsUpdate = true;
 
@@ -179,7 +153,7 @@ angular.module('edGalaxyMap')
 
 					function getPopulationScaleForSystem(system) {
 						if (system.population) {
-							return BASE_POINT_SIZE * Math.max(system.population / POP_SIZE_THRESHOLD, 1.0);
+							return 50 * Math.max(system.population / POP_SIZE_THRESHOLD, 1.0);
 						}
 						return BASE_POINT_SIZE;
 					}
@@ -426,21 +400,20 @@ angular.module('edGalaxyMap')
 							selectedSystemIcon.translateY( + 0.7 );
 						}
 						requestAnimationFrame(animate);
-            TWEEN.update(time);
+						TWEEN.update(time);
 						render();
 					}
 
 					//
 					function render() {
-
-							var delta = clock.getDelta();
-							if(isLoading){
-								uniforms.time.value += delta * 5;
-							}
-	            renderer.autoClear = false;
-	            renderer.clear();
-	            // renderer.render(backgroundScene , backgroundCamera )
-							renderer.render(window.scene, camera);
+						var delta = clock.getDelta();
+						if(isLoading){
+							uniforms.time.value += delta * 5;
+						}
+						renderer.autoClear = false;
+						renderer.clear();
+						// renderer.render(backgroundScene , backgroundCamera )
+						renderer.render(window.scene, camera);
 					}
 				}
 			}
