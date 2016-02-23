ec10558d964d1ab62c1db5b28835401873db3970
Patrick Burns <pburns@kineticsupply.com>
2016-02-23 16:50:32 -0600

    updated package json and made changes for it to work

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/edgalaxy2-dev'
+    uri: 'mongodb://localhost/edGalaxy'
   },
   sequelize: {
     uri: 'sqlite://',
@@ -18,5 +18,5 @@ module.exports = {
     }
   },
 
-  seedDB: true
+  seedDB: false
 };
